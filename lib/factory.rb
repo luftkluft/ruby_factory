6556299728fd1ebfab0ea43fc7a0753dc1a20e26
luftkluft <luftkluft@gmail.com>
2018-11-08 14:45:46 +0300

    feature/dev: factory structure definition

--- lib/factory.rb
+++ lib/factory.rb
@@ -1,1 +1,12 @@
 # frozen_string_literal: true
+
+class Factory
+  def self.new(*keys, &block)
+    raise ArgumentError, 'Wrong number of arguments (0 for 1+)' if keys.empty?
+
+    Class.new do
+      attr_accessor(*keys)
+      class_eval(&block) if block_given?
+    end
+  end
+end
