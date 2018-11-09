ca5baf56d169462e9f8565b8b8c6309513464b7a
luftkluft <32194302+luftkluft@users.noreply.github.com>
2018-11-10 00:45:57 +0300

    Update factory.rb
    Code refactoring: added '[]=' setter (lines 26-30), used  guarded clause, added generation of exceptions for passing rspec test (line 12).

--- lib/factory.rb
+++ lib/factory.rb
@@ -2,27 +2,31 @@
 
 class Factory
   def self.new(*keys, &block)
-    raise ArgumentError, 'Wrong number of arguments (0 for 1+)' if keys.empty?
+    raise ArgumentError, 'Empty initialize' if keys.empty?
 
     Class.new do
       attr_accessor(*keys)
       class_eval(&block) if block_given?
 
       define_method :initialize do |*keys_data|
+        raise ArgumentError, 'Mismatch number of arguments' if keys.count != keys_data.count
+
         keys.zip(keys_data).each do |arg, value|
-          unless arg.is_a? Symbol
-            raise NameError, "identifier #{arg} must be constant"
-          end
+          raise NameError, "identifier #{arg} must be constant" unless arg.is_a? Symbol
 
           send("#{arg}=", value)
         end
 
         def [](arg)
-          if (arg.is_a? String) || (arg.is_a? Symbol)
-            instance_variable_get("@#{arg}")
-          elsif arg.is_a? Integer
-            instance_variable_get(instance_variables[arg])
-          end
+          return instance_variable_get(instance_variables[arg]) if arg.is_a?(Integer)
+
+          instance_variable_get("@#{arg}")
+        end
+
+        def []=(arg, value)
+          return instance_variable_set(instance_variables[arg], value) if arg.is_a?(Integer)
+
+          instance_variable_set(:"@#{arg}", value)
         end
       end
     end
