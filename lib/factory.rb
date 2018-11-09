b72aa16358687bf0ae66f180aaa2088846e6accb
luftkluft <luftkluft@gmail.com>
2018-11-09 23:29:17 +0300

    feature/pull_2: refactoring by rubocop

--- lib/factory.rb
+++ lib/factory.rb
@@ -2,29 +2,31 @@
 
 class Factory
   def self.new(*keys, &block)
-    raise ArgumentError, 'Wrong number of arguments (0 for 1+)' if keys.empty?
+    raise ArgumentError, 'Empty initialize' if keys.empty?
 
     Class.new do
       attr_accessor(*keys)
       class_eval(&block) if block_given?
 
       define_method :initialize do |*keys_data|
-        raise ArgumentError, 'Wrong number of arguments (the number of arguments does not match)' if keys.count != keys_data.count
+        raise ArgumentError, 'Mismatch number of arguments' if keys.count != keys_data.count
+
         keys.zip(keys_data).each do |arg, value|
-          unless arg.is_a? Symbol
-            raise NameError, "identifier #{arg} must be constant"
-          end
+          raise NameError, "identifier #{arg} must be constant" unless arg.is_a? Symbol
+
           send("#{arg}=", value)
         end
 
         def [](arg)
           return instance_variable_get(instance_variables[arg]) if arg.is_a?(Integer)
-          return instance_variable_get("@#{arg}")
+
+          instance_variable_get("@#{arg}")
         end
 
         def []=(arg, value)
           return instance_variable_set(instance_variables[arg], value) if arg.is_a?(Integer)
-          return instance_variable_set(:"@#{arg}", value)
+
+          instance_variable_set(:"@#{arg}", value)
         end
       end
     end
