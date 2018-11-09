c17f32da1a5cdfb1175c9c5b6b8d0ff4bff2b5b9
luftkluft <luftkluft@gmail.com>
2018-11-09 23:33:10 +0300

    feature/pull_2: lib/main.rb add full test

--- lib/main.rb
+++ lib/main.rb
@@ -18,3 +18,10 @@ Customer2 = Factory.new(:name, :address) do
 end
 
 p Customer2.new('Dave', '123 Main').greeting
+
+Customer3 = Factory.new(:name, :address, :zip)
+joe = Customer3.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
+joe['name'] = 'Luke'
+joe[:zip]   = '90210'
+p joe.name
+p joe.zip
