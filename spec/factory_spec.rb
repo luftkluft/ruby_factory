59dfc7951fd538b8f55e9a93675c778139d9624e
luftkluft <luftkluft@gmail.com>
2018-11-09 03:25:41 +0300

    feature/pull_2: tuning rspec test 6

--- spec/factory_spec.rb
+++ spec/factory_spec.rb
@@ -60,12 +60,9 @@ RSpec.describe 'Factory' do
   # test 6
   it 'attribute assignment operator works as expected' do
     Customer = Factory.new(:name, :address, :zip)
-
     joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
-
     joe['name'] = 'Luke'
     joe[:zip]   = '90210'
-
     expect(joe.name).to eq('Luke')
     expect(joe.zip).to eq('90210')
   end
