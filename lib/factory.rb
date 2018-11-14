15f35f0bd4276f958cdd8fb747944667ed307407
luftkluft <32194302+luftkluft@users.noreply.github.com>
2018-11-14 22:18:00 +0300

    Update factory.rb
    Reworked.

--- lib/factory.rb
+++ lib/factory.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
-require_relative 'methods'
-
 class Factory
   def self.new(*keys, &block)
     raise ArgumentError, 'Empty initialize' if keys.empty?
 
-    if keys[0].is_a? String
-      const_set(keys.shift.capitalize, get_class(*keys, &block))
+    if keys.first.is_a? String
+      const_set(keys.shift.capitalize, build_class(*keys, &block))
     else
-      get_class(*keys, &block)
+      build_class(*keys, &block)
     end
   end
 
-  def self.get_class(*keys, &block)
+  def self.build_class(*keys, &block)
     Class.new do
       attr_accessor(*keys)
       class_eval(&block) if block_given?
@@ -23,7 +21,80 @@ class Factory
 
         keys.zip(keys_data).each { |arg, value| send("#{arg}=", value) }
       end
-      include Methods
+
+      def [](arg)
+        return instance_variable_get(instance_variables[arg]) if arg.is_a?(Integer)
+
+        instance_variable_get("@#{arg}")
+      end
+
+      def []=(arg, value)
+        instance_variable_set(instance_variables[arg], value) if arg.is_a?(Integer)
+        instance_variable_set(:"@#{arg}", value)
+      end
+
+      def ==(other)
+        compare(other) { |ffalse, sself, oother| ffalse && (sself == oother) }
+      end
+
+      def compare(other)
+        instance_variables.inject(true) do |ffalse, variable|
+          return false unless ffalse
+
+          begin
+            sself = instance_variable_get(variable)
+            oother = other.instance_variable_get(variable)
+            yield(ffalse, sself, oother) if block_given?
+          rescue StandardError
+            false
+          end
+        end
+      end
+
+      def dig(*args)
+        to_h.dig(*args)
+      end
+
+      def to_h
+        Hash[instance_variables.map do |name|
+          [name.to_s.delete('@').to_sym,
+           instance_variable_get(name)]
+        end]
+      end
+
+      def each(&value)
+        to_a.each(&value)
+      end
+
+      def to_a
+        instance_variables.map { |variable| instance_variable_get(variable) }
+      end
+
+      def each_pair(&name_value)
+        to_h.each_pair(&name_value)
+      end
+
+      def length
+        to_a.size
+      end
+
+      alias_method :size, :length
+
+      def members
+        instance_variables.map { |member| member.to_s.delete('@').to_sym }
+      end
+
+      def select(&member_value)
+        to_a.select(&member_value)
+      end
+
+      def values_at(*indexes)
+        indexes.map do |index|
+          raise IndexError unless instance_variables[index]
+
+          to_a[index]
+        end
+      end
     end
   end
 end
