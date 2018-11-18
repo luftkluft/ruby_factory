df26d8f0baedb0290ed44b100667cd265be59ffe
luftkluft <32194302+luftkluft@users.noreply.github.com>
2018-11-18 14:03:05 +0300

    Update factory.rb
    Reworked.

--- lib/factory.rb
+++ lib/factory.rb
@@ -1,97 +1,105 @@
 # frozen_string_literal: true
 
 class Factory
-  def self.new(*keys, &block)
-    raise ArgumentError, 'Empty initialize' if keys.empty?
+  class << self
+    include Validator
 
-    if keys.first.is_a? String
-      const_set(keys.shift.capitalize, build_class(*keys, &block))
-    else
-      build_class(*keys, &block)
+    def new(*keys, &block)
+      check_for_emptiness(keys)
+      if keys.first.is_a? String
+        const_set(keys.shift.capitalize, build_class(*keys, &block))
+      else
+        build_class(*keys, &block)
+      end
     end
-  end
 
-  def self.build_class(*keys, &block)
-    Class.new do
-      attr_accessor(*keys)
-      class_eval(&block) if block_given?
+    def build_class(*keys, &block)
+      Class.new do
+        attr_accessor(*keys)
+        include Validator
 
-      define_method :initialize do |*keys_data|
-        raise ArgumentError, 'Mismatch number of arguments' if keys.count != keys_data.count
+        define_method :initialize do |*keys_value|
+          chech_for_symbol(*keys)
+          check_for_match_argument_count(keys, keys_value)
+          keys.zip(keys_value).each { |key, value| public_send("#{key}=", value) }
+        end
 
-        keys.zip(keys_data).each { |arg, value| send("#{arg}=", value) }
-      end
+        def [](arg)
+          return instance_variable_get(instance_variables[arg]) if arg.is_a?(Integer)
 
-      def [](arg)
-        return instance_variable_get(instance_variables[arg]) if arg.is_a?(Integer)
+          instance_variable_get("@#{arg}")
+        end
 
-        instance_variable_get("@#{arg}")
-      end
+        def []=(arg, value)
+          arg_value = arg.is_a?(Integer) ? instance_variables[arg] : :"@#{arg}"
+          instance_variable_set(arg_value, value)
+        end
 
-      def []=(arg, value)
-        instance_variable_set(instance_variables[arg], value) if arg.is_a?(Integer)
-        instance_variable_set(:"@#{arg}", value)
-      end
-
-      def ==(other)
-        compare(other) { |ffalse, sself, oother| ffalse && (sself == oother) }
-      end
-
-      def compare(other)
-        instance_variables.inject(true) do |ffalse, variable|
-          return false unless ffalse
-
-          begin
-            sself = instance_variable_get(variable)
-            oother = other.instance_variable_get(variable)
-            yield(ffalse, sself, oother) if block_given?
-          rescue StandardError
-            false
+        def ==(other)
+          compare(other) do |result_of_compare, variable_of_first_object, variable_of_other|
+            result_of_compare && (variable_of_first_object == variable_of_other)
           end
         end
-      end
 
-      def dig(*args)
-        to_h.dig(*args)
-      end
+        def dig(*path)
+          path.inject(self) do |key, value|
+            return nil if key[value].nil?
 
-      def to_h
-        Hash[instance_variables.map do |name|
-          [name.to_s.delete('@').to_sym,
-           instance_variable_get(name)]
-        end]
-      end
+            key[value]
+          end
+        end
 
-      def each(&value)
-        to_a.each(&value)
-      end
+        def to_h
+          instance_variables.map { |variable| variable.to_s.delete('@').to_sym }.zip(to_a).to_h
+        end
 
-      def to_a
-        instance_variables.map { |variable| instance_variable_get(variable) }
-      end
+        def each(&value)
+          to_a.each(&value)
+        end
 
-      def each_pair(&name_value)
-        to_h.each_pair(&name_value)
-      end
+        def to_a
+          instance_variables.map { |variable| instance_variable_get(variable) }
+        end
 
-      def length
-        to_a.size
-      end
+        def each_pair(&name_value)
+          to_h.each_pair(&name_value)
+        end
 
-      alias_method :size, :length
+        def length
+          to_a.size
+        end
 
-      def members
-        instance_variables.map { |variable| variable.to_s.delete('@').to_sym }
-      end
+        def members
+          to_h.keys
+        end
 
-      def select(&member_value)
-        to_a.select(&member_value)
-      end
+        def select(&member_value)
+          to_a.select(&member_value)
+        end
 
-      def values_at(*indexes)
-        indexes.map do |index|
+        def values_at(*indexes)
+          indexes.map { |index| to_a[index] }
+        end
 
-          to_a[index]
+        alias_method :size, :length
+        alias_method :eql?, :==
+        alias_method :values, :to_a
+        class_eval(&block) if block_given?
+
+        private
+
+        def compare(other)
+          instance_variables.inject(true) do |result_of_compare, variable|
+            return false unless result_of_compare
+
+            begin
+              variable_of_first_object = instance_variable_get(variable)
+              variable_of_other = other.instance_variable_get(variable)
+              yield(result_of_compare, variable_of_first_object, variable_of_other) if block_given?
+            rescue StandardError
+              false
+            end
+          end
         end
       end
     end
